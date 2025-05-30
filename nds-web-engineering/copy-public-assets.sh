8b1f2a0b82065df6bea09855ac6419e72b9483b7
Hannes Morgenthaler <hanneszbinden@outlook.com>
2025-05-30 02:49:02 +0200

    update content of day-1.

--- nds-web-engineering/copy-public-assets.sh
+++ nds-web-engineering/copy-public-assets.sh
@@ -4,5 +4,7 @@ set -e
 
 SLIDEV_FOLDER="../../slides-nds-web-engineering"
 SLIDES_IMAGES_FOLDER_NAME="slides-images"
+SLIDES_ASSETS_FOLDER_NAME="web-assets"
 
 cp --recursive --update "$SLIDEV_FOLDER/public/$SLIDES_IMAGES_FOLDER_NAME/." "../$SLIDES_IMAGES_FOLDER_NAME"
+cp --recursive --update "$SLIDEV_FOLDER/public/$SLIDES_ASSETS_FOLDER_NAME/." "../$SLIDES_ASSETS_FOLDER_NAME"
