afb9f62d6eb7e2df29f1e4db46bc6f32d74f5802
Hannes Morgenthaler <10463536+mumrich@users.noreply.github.com>
2025-07-04 11:19:15 +0200

    update for day 6.

---
+++ nds-web-engineering/day-1/slidev/assets/slidev/default-DZX4pA6K.js
@@ -0,0 +1,1 @@
+import{f as o,o as t,j as s}from"../modules/vue-FfmNKdLO.js";import{_ as r}from"../index-DbDy2Y6P.js";const c={},a={class:"slidev-layout default"};function n(e,l){return t(),o("div",a,[s(e.$slots,"default")])}const f=r(c,[["render",n]]);export{f as I};
