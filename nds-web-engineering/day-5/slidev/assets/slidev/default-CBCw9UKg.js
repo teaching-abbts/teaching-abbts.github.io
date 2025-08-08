1d13d56a5e7ffc5691cd74f8e5269b552a5edcb8
Hannes Morgenthaler <hanneszbinden@outlook.com>
2025-08-08 11:44:33 +0200

    update for day 7

---
+++ nds-web-engineering/day-5/slidev/assets/slidev/default-CBCw9UKg.js
@@ -0,0 +1,1 @@
+import{f as o,o as t,j as s}from"../modules/vue-BeHGrh2X.js";import{_ as r}from"../index-CYi4_kSE.js";const c={},a={class:"slidev-layout default"};function n(e,l){return t(),o("div",a,[s(e.$slots,"default")])}const f=r(c,[["render",n]]);export{f as I};
