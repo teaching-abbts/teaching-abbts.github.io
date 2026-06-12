faa2178fbc756edcd03353647b76841e2068b1cb
Hannes Morgenthaler <hmo@ingtes.ch>
2026-06-12 10:31:16 +0200

    final updates for 2025

---
+++ nds-web-engineering/day-1/slidev/assets/slidev/default-DTdEGk0j.js
@@ -0,0 +1,1 @@
+import{f as o,o as t,j as s}from"../modules/vue-BO_CY555.js";import{_ as r}from"../index-DwSY5Nee.js";const c={},a={class:"slidev-layout default"};function n(e,l){return t(),o("div",a,[s(e.$slots,"default")])}const f=r(c,[["render",n]]);export{f as I};
