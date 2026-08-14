9cdf56e5a750f0eb39796b08c7c7c13b83d7e609
Hannes Morgenthaler <hmo@inges.ch>
2026-08-14 12:05:44 +0200

    updated slides.

---
+++ nds-web-engineering/2026/chapter-8/slidev/assets/swimlanesDiagram-GLBMEWQJ-Bf9nAsVm.js
@@ -0,0 +1,8 @@
+import{r as e}from"./chunk-PTVI3W5X-DVfdoxm6.js";import"./chunk-FO5PYUIK-CPzj2963.js";import"./chunk-NEZ6ONQZ-Ll-t4EtS.js";import"./chunk-PWCFYZI5-Dz1excih.js";import"./chunk-PLCLPJVV-BfMyJHbe.js";import"./chunk-XXV7AQMY---ONizwt.js";import"./chunk-IPM4HZQ6-CQmHNgJe.js";import"./chunk-OX6ZKUVE-D5ltN9S1.js";import"./chunk-YV36FAQU-hpECZzaf.js";import"./chunk-AQ2D6KKF-DoSmn5J1.js";import"./chunk-2V3FENDJ-BN7y1wvm.js";import"./chunk-DKH7L6O3-9BaSQTXj.js";import"./chunk-RTI7CJYH-CrTK6TD7.js";import"./chunk-MPM6TOF7-DJ4b9QZo.js";import"./chunk-STSWKLMO-Dgx2Hw2Z.js";import"./chunk-ZICDAICJ-BsskO1_7.js";import"./chunk-HPLX5OYV-zid2YVw5.js";import"./chunk-BYAF6FXG-Dw0xcCBH.js";import{r as t,t as n}from"./chunk-MX3EQCGM-twln9dhH.js";var r=n({defaultLayout:`swimlane`,styles:e(e=>`${t(e)}
+  .swimlane.cluster rect {
+    stroke: ${e.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,`getStyles`)});export{r as diagram};
